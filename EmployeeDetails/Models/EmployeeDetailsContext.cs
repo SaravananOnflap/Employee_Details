4e6ae2b92100c66c0bfb6315103bbfb9c5bea8b1
SaravananOnflap <saro4221@gmail.com>
2024-03-03 10:32:06 -0800

    latestChanges

---
+++ EmployeeDetails/Models/EmployeeDetailsContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeDetails.Models
+{
+    public class EmployeeDetailsContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public EmployeeDetailsContext() : base("name=EmployeeDetailsContext")
+        {
+             //   public System.Data.Entity.DbSet<EmployeeDetails.Models.Employee> Employees { get; set; }
+        }
+
+        public DbSet<EmployeeDetails.Models.Employee> tbl_EmployeeDetails { get; set; }
+
+    }
+}
---
+++ EmployeeDetails/Scripts/CRUD/CRUD.js
@@ -0,0 +1,253 @@
+﻿  $('#loginvalidate').click(function () {
+        var data = {
+            "username": $("#username").val(),
+            "password": $("#password").val()
+        };
+        $.ajax({
+            url: "/Home/validateuser",
+            type: "POST",
+            data: JSON.stringify(data),
+            dataType: "json",
+            contentType: "application/json",
+            success: function (response) {
+                
+                if (response.Success) {
+                    Swal.fire({
+                        icon: 'Success',
+                        title: 'Success',
+                        text: 'Login Successfully!',
+                        timer: 3000,
+                        timerProgressBar: true,
+                        showConfirmButton: false
+                    }).then(() => {
+                        window.location.href = "/Employee/Index";
+                    });
+                }
+                else {
+                    Swal.fire({
+                        icon: 'Failed',
+                        title: 'Failed',
+                        text: 'Invalid Username Password!',
+                        timer: 3000,
+                        timerProgressBar: true,
+                        showConfirmButton: false
+                    }).then(() => {
+                        
+                    });
+                }
+            },
+            error: function (e) {
+                Swal.fire({
+                    icon: 'Failed',
+                    title: 'Failed',
+                    text: 'Invalid Username Password!',
+                    timer: 3000,
+                    timerProgressBar: true,
+                    showConfirmButton: false
+                }).then(() => {
+
+                });
+            }
+        });
+
+  });
+
+
+
+function EmpDelete(id) {
+    
+    $.ajax({
+        url: "/Employee/Delete",
+        type: "POST",
+        data: { id: id },
+        success: function (response) {
+            if (response) {
+                Swal.fire({
+                    icon: 'Delete',
+                    title: 'Delete',
+                    text: 'Employee Detail Deleted Successfully!',
+                    timer: 3000,
+                    timerProgressBar: true,
+                    showConfirmButton: false
+                }).then(() => {
+                    window.location.href = "/Employee/Index";
+                });
+            }
+        },
+        error: function (e) {
+            window.location.href = "/Employee/Index";
+        }
+    });
+}
+function EditEmployee(id) {
+    debugger;
+    var EmpID=id;
+    var EmployeeName = $("#EmployeeName").val();
+    var PhoneNumber = $("#PhoneNumber").val();
+    var Description = $("#Description").val();
+    var EmailID = $("#EmailID").val();
+    var UserID = $("#UserID").val();
+    var Pwd = $("#Pwd").val();
+    var EmpRank = $("#EmpRank").val();
+
+
+    if (EmployeeName == "" || EmployeeName == "undefined") {
+        $("#EmployeeName").focus();
+        return false;
+    }
+    if (UserID == "" || UserID == "undefined") {
+        $("#UserID").focus();
+        return false;
+    }
+    if (Pwd == "" || Pwd == "undefined") {
+        $("#Pwd").focus();
+        return false;
+    }
+
+    var data = {
+
+        "EmpID": EmpID,
+        "EmployeeName": EmployeeName,
+        "PhoneNumber": PhoneNumber,
+        "Description": Description,
+        "EmailID": EmailID,
+        "UserID": UserID,
+        "Pwd": Pwd,
+        "EmpRank": EmpRank,
+
+    };
+    $.ajax({
+        url: "/Employee/EditEmployee",
+        type: "POST",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        success: function (response) {
+            if (response) {
+                console.log(response);
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Saved',
+                    text: 'Employee has been saved successfully',
+                    timer: 3000,
+                    timerProgressBar: true,
+                    showConfirmButton: false
+                }).then(() => {
+                    window.location.href = "/Employee/Index";
+                });
+            }
+
+        },
+        error: function (e) {
+            Swal.fire({
+                icon: 'Failed',
+                title: 'Failed',
+                text: 'Employee Details Saved Failed',
+                timer: 3000,
+                timerProgressBar: true,
+                showConfirmButton: false
+            }).then(() => {
+                window.location.href = "/Employee/Index";
+            });
+        }
+    });
+
+
+
+
+  
+}
+
+
+$(document).ready(function () {
+    $('#PhoneNumber').on('input', function () {
+        var phone = $(this).val();
+        if (phone.length > 10 || isNaN(phone)) {
+            $(this).val('');
+        }
+    });
+});
+
+
+function validateEmail() {
+    var email = document.getElementById("EmailID").value;
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email)) {
+        alert("Invalid email format.");
+        return false;
+    }
+    return true;
+}
+
+
+
+$('#SaveEmployee').click(function () {
+
+  
+    var EmployeeName = $("#EmployeeName").val();   
+    var PhoneNumber = $("#PhoneNumber").val();
+    var Description = $("#Description").val();
+    var EmailID = $("#EmailID").val();
+    var UserID = $("#UserID").val();
+    var Pwd = $("#Pwd").val();
+    var EmpRank = $("#EmpRank").val();
+   
+
+    if (EmployeeName == "" || EmployeeName == "undefined") {
+        $("#EmployeeName").focus();
+        return false;
+    }
+    if (UserID == "" || UserID == "undefined") {
+        $("#UserID").focus();
+        return false;
+    }
+    if (Pwd == "" || Pwd == "undefined") {
+        $("#Pwd").focus();
+        return false;
+    }
+    
+    var data = {
+        "EmployeeName": EmployeeName,
+        "PhoneNumber": PhoneNumber,
+        "Description": Description,
+        "EmailID": EmailID,
+        "UserID": UserID,
+        "Pwd": Pwd,
+        "EmpRank": EmpRank,
+        
+    };
+    $.ajax({
+        url: "/Employee/InsertData",
+        type: "POST",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        success: function (response) {
+            if (response) {
+                console.log(response);
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Saved',
+                    text: 'Employee has been saved successfully',
+                    timer: 3000,
+                    timerProgressBar: true,
+                    showConfirmButton: false
+                }).then(() => {
+                    window.location.href = "/Employee/Index";
+                });
+            }
+
+        },
+        error: function (e) {
+            Swal.fire({
+                icon: 'Failed',
+                title: 'Failed',
+                text: 'Employee Details Saved Failed',
+                timer: 3000,
+                timerProgressBar: true,
+                showConfirmButton: false
+            }).then(() => {
+                window.location.href = "/Employee/Index";
+            });
+        }
+    });
+});
+
